d6740474feef89d46856311931f70dca94832f63
Bianca-PT <bianca.kisten@playful.today>
2026-06-30 20:49:09 +0200

    configure Tailwind CSS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <>
-      <h1>AI Resume Assistant</h1>
-    </>
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-600">
+        AI Resume Assistant
+      </h1>
+    </div>
   );
 }
 
